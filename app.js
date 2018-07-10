abfa0ad419a8892de3b0bdbdd4b31837aed77703
michellehkim <michellehkim@github.com>
2018-07-10 16:59:15 -0700

    added delete button

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 function onReady() {
   const addToDoForm = document.getElementById('addToDoForm');
-   const newToDoText = document.getElementById('newToDoText');
-   const toDoList = document.getElementById('toDoList');
+  const newToDoText = document.getElementById('newToDoText');
+  const toDoList = document.getElementById('toDoList');
 
    addToDoForm.addEventListener('submit', event => {
      event.preventDefault();
@@ -12,20 +12,31 @@ function onReady() {
   let newLi = document.createElement('li');
   // create a new input
   let checkbox = document.createElement('input');
+  //add create delte deletebutton
+  let deletebtn = document.createElement('button');
+  //create text node
+  let deleteText = document.createTextNode('delete');
   // set the input's type to checkbox
   checkbox.type = "checkbox";
-  });
-  // set the title
+  // set the titlel
   newLi.textContent = title;
   // attach the checkbox to the li
   newLi.appendChild(checkbox);
   //attach deletebutton to li
   newLi.appendChild(deletebtn);
+  //attach text to deletebutton
+  newLi.appendChild(deleteText);
   // attach the li to the ul
   toDoList.appendChild(newLi);
   //empty the input
   newToDoText.value = '';
 
- }window.onload = function() {
+  deletebtn.addEventListener('click', function() {
+         toDoList.removeChild(newLi);
+     });
+  });
+ }
+
+ window.onload = function() {
   onReady();
  };
